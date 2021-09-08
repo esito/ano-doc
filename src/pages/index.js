e3a252d4e7be64d84f54115acf0c48edb50ecdec
Martin Holøs <mhesito@gmail.com>
2021-09-08 02:24:46 +0200

    removed homepage header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,13 @@
 import React from 'react'
-import clsx from 'clsx'
 import Layout from '@theme/Layout'
-import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import styles from './index.module.css'
-import HomepageFeatures from '../components/HomepageFeatures'
-
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext()
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  )
-}
+import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext()
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-
+    <Layout title={`Hello from ${siteConfig.title}`} description="Anonymize your test data and make GDPR a breeze">
+      <Redirect to="/docs/intro" />
     </Layout>
   )
 }
